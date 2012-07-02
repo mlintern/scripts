573b0a874a77c20e484044373eee853567c964cd
Mark Lintern <mlintern@compendium.com>
2012-07-02 17:38:03 +0000

    adding systems to update script

--- update_projects.sh
+++ update_projects.sh
@@ -2,23 +2,26 @@
 
 cd /home/developer/blog/application/trunk
 git co master
-git pull
+git pull -u
 cd /home/developer/presentation/application/trunk
 git co master
-git pull
+git pull -u
 cd /home/developer/sitemap/application/trunk
 git co master
-git pull
+git pull -u
 cd /home/developer/callback/application/trunk
 git co master
-git pull
+git pull -u
 cd /home/developer/template_manager
 git co master
-git pull
+git pull -u
 cd /home/developer/image_manager
 git co master
-git pull
+git pull -u
 cd /home/developer/shared
 git co master
-git pull
+git pull -u
+cd /home/developer/systems
+git co master
+git pull -u
 
