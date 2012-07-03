7ca56a6dc010fd7b4680299873e44122217e0b08
Mark Lintern <mlintern@compendium.com>
2012-07-03 16:26:36 +0000

    added progress bar scripts

--- tmux-script-management/collectamazonserverinfo.sh
+++ tmux-script-management/collectamazonserverinfo.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ec2-describe-instances | grep "Name\|INSTANCE" | perl -pe 's/\s+/:/g' | perl -pe 's/INSTA/\nINSTA/g' | perl -pe 's/sg-[^:]+://g' | awk -F: '{print $26,",",$4,",",$13}' > ~/FullServerList.csv
 export EC2_PRIVATE_KEY=~/.ec2/eng-testing/pk-EL6OCE7KGWXPLWSWQXRAU6V3AQ45HPJ4.pem
 export EC2_CERT=~/.ec2/eng-testing/cert-EL6OCE7KGWXPLWSWQXRAU6V3AQ45HPJ4.pem
