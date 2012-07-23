870556ea18dd52fd0354294f71910b00cbc189bf
Mark Lintern <mlintern@compendium.com>
2012-07-23 14:10:39 +0000

    Updated Order and Added labels

--- update_projects.sh
+++ update_projects.sh
@@ -1,26 +1,34 @@
 #!/bin/sh
 
+echo 'Updating Blog'
 cd /home/developer/blog/application/trunk
 git co master
 git pull -u
+echo 'Updating Presentation'
 cd /home/developer/presentation/application/trunk
 git co master
 git pull -u
-cd /home/developer/sitemap/application/trunk
-git co master
-git pull -u
-cd /home/developer/callback/application/trunk
-git co master
-git pull -u
+echo 'Updating Template Manager'
 cd /home/developer/template_manager
 git co master
 git pull -u
+echo 'Updating Image Manager'
 cd /home/developer/image_manager
 git co master
 git pull -u
+echo 'Updating Sitemap'
+cd /home/developer/sitemap/application/trunk
+git co master
+git pull -u
+echo 'Updating Callback'
+cd /home/developer/callback/application/trunk
+git co master
+git pull -u
+echo 'Updating Shared'
 cd /home/developer/shared
 git co master
 git pull -u
+echo 'Updating Systems'
 cd /home/developer/systems
 git co master
 git pull -u
