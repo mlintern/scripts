ae1dea0061bdfc96a2774f7a8c22997663d921cd
Mark Lintern <markllintern@gmail.com>
2014-10-13 16:52:23 -0400

    added the rest of the repos to the remote update

--- update_git_remote.sh
+++ update_git_remote.sh
@@ -2,11 +2,12 @@
 
 
 start_dir='/Users/mlintern/dev/'
-dir_list=('blog/application/trunk' 'padrino_api' 'systems' 'callback/application/trunk' 'sitemap' 'content_router' 'shared' 'gettington' )
+dir_list=('blog/application/trunk' 'padrino_api' 'systems' 'callback/application/trunk' 'sitemap' 'content_router' 'shared' 'gettington' 'ProductSupport' 'Website' 'kanhub' 'presentation/application/trunk' 'hubot' 'wordpress-import' 'image_manager' 'BlueLantern' 'maeby' 'analytics-collector' 'analytics-processor' 'cpdm-es-plugins' 'WordpressIntegration' 'ContentGrader' 'CompendiumOSN' 'osn-notifiier' 'Selenium Tests' 'rss-fetcher-cpdm' 'MetaConfigProxy' 'LocalProxy' 'yard-compendium')
 
 for i in "${dir_list[@]}"
 do
 	dir=$start_dir$i
+	printf "\n\n\n"
 	echo $dir
 	cd $dir
 	git checkout master
