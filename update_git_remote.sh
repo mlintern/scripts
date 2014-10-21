5846b2923815838015fb1a148a5e746c38173420
Mark Lintern <markllintern@gmail.com>
2014-10-21 14:48:38 -0400

    more output from scriptwq

--- update_git_remote.sh
+++ update_git_remote.sh
@@ -2,7 +2,9 @@
 
 
 start_dir='/Users/mlintern/dev/'
-dir_list=('blog/application/trunk' 'padrino_api' 'systems' 'callback/application/trunk' 'sitemap' 'content_router' 'shared' 'gettington' 'ProductSupport' 'Website' 'kanhub' 'presentation/application/trunk' 'hubot' 'wordpress-import' 'image_manager' 'BlueLantern' 'maeby' 'analytics-collector' 'analytics-processor' 'cpdm-es-plugins' 'WordpressIntegration' 'ContentGrader' 'CompendiumOSN' 'osn-notifiier' 'Selenium Tests' 'rss-fetcher-cpdm' 'MetaConfigProxy' 'LocalProxy' 'yard-compendium')
+dir_list=('blog/application/trunk' 'padrino_api' 'systems' 'callback/application/trunk' 'sitemap' 'content_router' 'shared' 'gettington' 'ProductSupport' 'Website' 'kanhub' 'presentation/application/trunk' 'wordpress-import' 'image_manager' 'BlueLantern' 'maeby' 'analytics-collector' 'analytics-processor' 'cpdm-es-plugins' 'WordpressIntegration' 'ContentGrader' 'CompendiumOSN' 'osn-notifiier' 'Selenium-Tests' 'rss-fetcher-cpdm' 'MetaConfigProxy' 'LocalProxy' 'yard-compendium')
+
+echo "${#dir_list[@]} repos" 
 
 for i in "${dir_list[@]}"
 do
@@ -14,4 +16,3 @@ do
 	git fetch origin
 	git push --all orahub
 done
-
