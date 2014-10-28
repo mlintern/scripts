d0b07fb1b69eb4c1cbe092a65860a2c3eef87236
Mark Lintern <markllintern@gmail.com>
2014-10-28 13:45:55 -0400

    alias updatewq

--- update_git_remote.sh
+++ update_git_remote.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 
-start_dir='/Users/mlintern/dev/'
+start_dir='/Users/mlintern/dev-old/'
 dir_list=('blog/application/trunk' 'padrino_api' 'systems' 'callback/application/trunk' 'sitemap' 'content_router' 'shared' 'gettington' 'ProductSupport' 'Website' 'kanhub' 'presentation/application/trunk' 'wordpress-import' 'image_manager' 'BlueLantern' 'maeby' 'analytics-collector' 'analytics-processor' 'cpdm-es-plugins' 'WordpressIntegration' 'ContentGrader' 'CompendiumOSN' 'osn-notifiier' 'Selenium-Tests' 'rss-fetcher-cpdm' 'MetaConfigProxy' 'LocalProxy' 'yard-compendium')
 
 echo "${#dir_list[@]} repos" 
@@ -13,6 +13,6 @@ do
 	echo $dir
 	cd $dir
 	git checkout master
-	git fetch origin
+	git pull --all origin
 	git push --all orahub
 done
